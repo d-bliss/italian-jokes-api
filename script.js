a903eb02a6a8d03ae1f0fe2411aa4ffef3d93446
Bliss <danbliss80@gmail.com>
2023-06-08 10:03:10 -0700

    implement toggle to play the joke alound

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const fetchButton = document.getElementById("fetchButton");
     const subtypeSelect = document.getElementById("subtypeSelect");
     const jokeContainer = document.getElementById("jokeContainer");
+    const ttsToggle = document.getElementById("ttsToggle"); // get the toggle switch
 
     fetchButton.addEventListener("click", async () => {
         const subtype = subtypeSelect.value;
@@ -28,6 +29,27 @@ document.addEventListener("DOMContentLoaded", () => {
             <h2>${joke.subtype} Joke</h2>
             <p>${joke.joke}</p>
         `;
+        speakJoke(joke); // call the speakJoke function here
+    }
+
+    function speakJoke(joke) {
+        if (!ttsToggle.checked) {
+            return;
+        }
+        const utterance = new SpeechSynthesisUtterance(joke.joke);
+        utterance.rate = 0.9; // Adjust the speech rate
+
+        const voices = window.speechSynthesis.getVoices();
+        const selectedVoice = voices.find(
+            (voice) => voice.name === "Google UK English Female"
+        );
+
+        if (selectedVoice) {
+            utterance.voice = selectedVoice; // Use the selected voice
+        }
+        // If no matching voice is found, the default voice will be used
+
+        window.speechSynthesis.speak(utterance);
     }
 
     function displayError(message) {
